2f6dd12b32f28d48965f8f2eb6f65fbc18ef9d39
ESRI-SA\tmahlo <tmahlo@esri-southafrica.com>
2025-07-29 19:10:11 +0200

    endpoint

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -26,7 +26,7 @@ function Register() {
       return;
     }
     try {
-      const response = await fetch('/api/user/register', {
+            const response = await fetch('https://ctrlapiapp.azurewebsites.net/api/user/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
